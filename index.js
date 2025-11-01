6f7149706d6ab8be35505f59388068ec992242fb
migarcos <migark@hotmail.com>
2025-11-01 15:29:20 -0500

    get working

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.port || 8080;
 
-app.get( '/', (req, res) => {
-    res.send("The server is running now over render.com");
+const professionalRoutes = require("./routes/professional");
+
+// Equivalente a bodyParser.json()  from Express v4.16+ body-parser installation isn't necessary
+app.use(express.json()); 
+// app.use(express.urlencoded({ extended: true })); // Equivalente a bodyParser.urlencoded()
+
+// app.get( '/', (req, res) => {
+//     res.send("The server is running now over render.com");
+// });
+
+app.use( (req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
 });
+
+app.use("/professional", professionalRoutes);
 
 app.listen(port);
 
-console.log(`Server started on port ${port}`);
+// console.log(`Server started on port ${port}`);
