0ca6eea61ca6aec13ff88c7d94e6c170503cd6b5
migarcos <migark@hotmail.com>
2025-11-08 17:22:49 -0500

    swagger local success again-

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,6 +2,7 @@ const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
+    // #swagger.tags = ['Contacts']
     const result = await mongodb.getDatabase().db().collection('contact').find();
     result.toArray().then( (contacts) => {
         res.setHeader('Content-Type', 'application/json');
@@ -10,6 +11,7 @@ const getAll = async (req, res) => {
 }
 
 const getSingle = async (req, res) => {
+    // #swagger.tags = ['Contacts']
     const userId =  new ObjectId(req.params.id);
     const result = await mongodb.getDatabase().db().collection('contact').find({ _id: userId});
     result.toArray().then( (contacts) => {
@@ -19,22 +21,48 @@ const getSingle = async (req, res) => {
 }
 
 const createContact = async(req, res) => {
-    const contact = {
-        firstName : req.body.firstName,
-        lastname : req.body.lastname,
-        email : req.body.email,
-        favoriteColor : req.body.favoriteColor,
-        birthday : req.body.birthday
-    };
-    const response = await mongodb.getDatabase().db().collection('contact').insertOne(contact);
-    if (response.acknowledge) {
-        res.status(204).send;
-    } else {
-        res.status(500).json( response.Error || 'Some error ocurre updating contact');
+      // #swagger.tags = ['Contacts']
+    try {
+        const contact = {
+            firstName: req.body.firstName,
+            lastname: req.body.lastname,
+            email: req.body.email,
+            favoriteColor: req.body.favoriteColor,
+            birthday: req.body.birthday
+        };
+
+        const response = await mongodb
+        .getDatabase()
+        .db()
+        .collection('contact')
+        .insertOne(contact);
+
+        if (response.acknowledged) {
+            res.status(201).json({ message: 'Contact created successfully', id: response.insertedId });
+        } else {
+            res.status(400).json({ error: 'Failed to create contact' });
+        }
+    } catch (error) {
+        console.error('Error creating contact:', error);
+        res.status(500).json({ error: 'Internal server error' });
     }
+    // const contact = {
+    //     firstName : req.body.firstName,
+    //     lastname : req.body.lastname,
+    //     email : req.body.email,
+    //     favoriteColor : req.body.favoriteColor,
+    //     birthday : req.body.birthday
+    // };
+    // const response = await mongodb.getDatabase().db().collection('contact').insertOne(contact);
+    // if (response.acknowledged) {
+    //     res.status(204).send;
+    // } else {
+    //     res.status(500).json( response.Error || 'Some error ocurre updating contact');
+    // }
 };
 
 const updateContact = async(req, res) => {
+    // #swagger.tags = ['Contacts']
     const userId = new ObjectId(req.params.id);
     const contact = {
         firstName : req.body.firstName,
@@ -52,6 +80,7 @@ const updateContact = async(req, res) => {
 };
 
 const replaceContact = async(req, res) => {
+    // #swagger.tags = ['Contacts']
     const userId = new ObjectId(req.params.id);
     const contact = {
         firstName : req.body.firstName,
@@ -69,12 +98,31 @@ const replaceContact = async(req, res) => {
 };
 
 const deleteContact = async(req, res) => {
-    const userId = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().db().collection('contact').deleteOne( { _id: userId}, true);
-    if (response.deletedCount > 0) {
-        res.status(204).send;
-    } else {
-        res.status(500).json( response.Error || 'Some error ocurre updating contact');
+    // #swagger.tags = ['Contacts']
+    // const userId = new ObjectId(req.params.id);
+    // const response = await mongodb.getDatabase().db().collection('contact').deleteOne( { _id: userId}, true);
+    // if (response.deletedCount > 0) {
+    //     res.status(204).send;
+    // } else {
+    //     res.status(500).json( response.Error || 'Some error ocurre updating contact');
+    // }
+    try {
+        const userId = new ObjectId(req.params.id);
+
+        const response = await mongodb
+        .getDatabase()
+        .db()
+        .collection('contact')
+        .deleteOne({ _id: userId });
+
+        if (response.deletedCount > 0) {
+        res.status(204).send(); 
+        } else {
+        res.status(404).json({ error: 'Contact not found or already deleted' });
+        }
+    } catch (error) {
+        console.error('Error deleting contact:', error);
+        res.status(500).json({ error: 'Internal server error' });
     }
 };
 
--- index.js
+++ index.js
@@ -4,9 +4,22 @@ const mongodb = require('./data/database');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use( express.json() );
+app.use( (req, res, next) => { 
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type. Accept, Z-Key'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS'
+    );
+    next();
+} );
 app.use('/', require('./routes/contacts'));
-
 
 mongodb.initDb( (err) => {
     if (err) {
